12937f5c133a6f57d926d90562007231d9eb3533
Kep <fynjycloud@gmail.com>
2021-07-15 14:47:43 +0300

    fix problem with position of ofjects

--- config.js
+++ config.js
@@ -1,41 +1,41 @@
-export default {
-  "background": "https://i.ibb.co/QbdJM21/g4.jpg",
-  "objects": [
-    {
-      "top": 158,
-      "left": 205,
-      "width": 40,
-      "height": 93,
-      "url": "https://i.ibb.co/y4Rqzr2/zont1.png"
-    },
-    {
-      "top": 225,
-      "left": 78,
-      "width": 40,
-      "height": 66,
-      "url": "https://i.ibb.co/fCvR3qF/zont2.png"
-    },
-    {
-      "top": 330,
-      "left": 0,
-      "width": 100,
-      "height": 36,
-      "url": "https://i.ibb.co/FY32Y3s/zont3.png"
-    },
-    {
-      "top": 170,
-      "left": 130,
-      "width": 60,
-      "height": 170,
-      "url": "https://i.ibb.co/kJwfgH4/zont4.png"
-    },
-    {
-      "top": 326,
-      "left": 100,
-      "width": 70,
-      "height": 118,
-      "url": "https://i.ibb.co/0hxxWTv/zont5.png"
-    }
-  ],
-  "minFoundObjects": 3
+const config = {
+    "background": "https://i.ibb.co/QbdJM21/g4.jpg",
+    "objects": [
+        {
+            "top": 158,
+            "left": 205,
+            "width": 40,
+            "height": 93,
+            "url": "https://i.ibb.co/y4Rqzr2/zont1.png"
+        },
+        {
+            "top": 225,
+            "left": 78,
+            "width": 40,
+            "height": 66,
+            "url": "https://i.ibb.co/fCvR3qF/zont2.png"
+        },
+        {
+            "top": 330,
+            "left": 0,
+            "width": 100,
+            "height": 36,
+            "url": "https://i.ibb.co/FY32Y3s/zont3.png"
+        },
+        {
+            "top": 170,
+            "left": 130,
+            "width": 60,
+            "height": 170,
+            "url": "https://i.ibb.co/kJwfgH4/zont4.png"
+        },
+        {
+            "top": 326,
+            "left": 100,
+            "width": 70,
+            "height": 118,
+            "url": "https://i.ibb.co/0hxxWTv/zont5.png"
+        }
+    ],
+    "minFoundObjects": 3
 }
